d91892789575abf7a10502fec5b6de94664212da
Unknown <kevin.sauves@me.com>
2018-03-04 19:06:04 -0500

    first commit

---
+++ ToDo List/ToDo.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo.swift
+//  ToDo List
+//
+//  Created by Kevin Sauves on 2018-03-04.
+//  Copyright © 2018 Kevin Sauves. All rights reserved.
+//
+
+import UIKit
+
+
+class ToDo {
+    
+    var name = ""
+    var important = false
+    
+}
